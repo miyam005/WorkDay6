9948d287aa6b520702e8fbf50f34df0ebfc61ece
idsc3101 <idsc3101@gmail.com>
2014-10-06 15:25:11 -0500

    solved

--- IntersectionProblem/IntersectionProblem/Form1.vb
+++ IntersectionProblem/IntersectionProblem/Form1.vb
@@ -25,9 +25,29 @@ Public Class Form1
         C = CDbl(Cst)
         D = CDbl(Dst)
 
-        ' Please write your code here...
-
-        txtResult.Text = "Not implemented yet"
+        txtResult.Text = FindIntersection(A, B, C, D)
     End Sub
+
+        Function FindIntersection(A As Double, B As Double, C As Double, D As Double) As String
+        Dim result As String
+        If (B < C) Or (D < A) Then
+            result = "No intersection"
+        Else
+            Dim begPoint As Double
+            begPoint = Math.Max(A, C)
+
+            Dim endPoint As Double
+            endPoint = Math.Min(B, D)
+
+            result = "The intersection is from " & begPoint & " to " & endPoint
+        End If
+
+        txtResult.Text = result
+
+        Return result
+    End Function
+
+
+
 
 End Class
